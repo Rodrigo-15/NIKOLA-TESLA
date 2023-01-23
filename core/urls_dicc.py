892d05414a8b26367d43d5a2243b1a87ca76af27
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-01-23 11:09:54 -0500

    core/urls_dicc added with desk api urls

---
+++ core/urls_dicc.py
@@ -0,0 +1,34 @@
+urls_dicc = {
+    "desk": {
+        "headquarters": {
+            "get": "http://localhost:8000/desk/headquarters/",  # get
+            "save": "http://localhost:8000/desk/headquarters/",  # post
+            "delete": "http://localhost:8000/desk/headquarters/",  # delete
+            "edit": "http://localhost:8000/desk/headquarters/",  # put
+        },
+        "procedure_requirement": {
+            "get": "http://localhost:8000/desk/procedure_requirement/",  # get
+            "save": "http://localhost:8000/desk/procedure_requirement/",  # post
+            "delete": "http://localhost:8000/desk/procedure_requirement/",  # delete
+            "edit": "http://localhost:8000/desk/procedure_requirement/",  # put
+        },
+        "procedure_type": {
+            "get": "http://localhost:8000/desk/procedure_type/",  # get
+            "save": "http://localhost:8000/desk/procedure_type/",  # post
+            "delete": "http://localhost:8000/desk/procedure_type/",  # delete
+            "edit": "http://localhost:8000/desk/procedure_type/",  # put
+        },
+        "pocedure_procreqs": {
+            "get": "http://localhost:8000/desk/procedure_procreqs/",  # get
+            "save": "http://localhost:8000/desk/procedure_procreqs/",  # post
+            "delete": "http://localhost:8000/desk/procedure_procreqs/",  # delete
+            "edit": "http://localhost:8000/desk/procedure_procreqs/",  # put
+        },
+        "procedures": {
+            "get": "http://localhost:8000/desk/procedures/",  # get
+            "save": "http://localhost:8000/desk/procedures/",  # post
+            "delete": "http://localhost:8000/desk/procedures/",  # delete
+            "edit": "http://localhost:8000/desk/procedures/",  # put
+        },
+    },
+}
