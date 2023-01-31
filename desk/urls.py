b397c6f929fdddafea13e90bf82ef753343e6990
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-01-31 12:09:25 -0500

    unnecessary endpoints removed

--- desk/urls.py
+++ desk/urls.py
@@ -1,14 +1,12 @@
 from django.urls import include, path
 
 from desk.router import router
-from desk.views import get_procedures, create_procedure, login, update_procedure
+from desk.views import get_procedures, login
 
 app_name = "desk"
 
 urlpatterns = [
     path("", include(router.urls)),
     path("get_procedures", get_procedures, name="get_procedures"),
-    path("create_procedure/", create_procedure, name="create_procedure"),
-    path("update_procedure/", update_procedure, name="update_procedure"),
     path("login/", login, name="login"),
 ]
