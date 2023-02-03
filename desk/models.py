504d6048be806fee007047dfd0673f0af837fe43
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-02-03 09:36:55 -0500

    model edited

--- desk/models.py
+++ desk/models.py
@@ -108,8 +108,8 @@ class Procedure(models.Model):
         return Procedure.objects.filter(id=procedure_id)
 
     def __str__(self):
-        return f"{self.file_id} - {self.subject} - {self.procedure_type} -\
-                {self.reference_doc_number} - {self.headquarter}"
+        return f"{self.file_id} - {self.code_number} - {self.subject} - {self.procedure_type} -\
+                 {self.reference_doc_number} - {self.headquarter}"
 
 
 class Procedure_ProcReq(models.Model):
