5302fdecbc18bc9756647153fbb03d8875f952ce
Carlos Casado Vargas <carloscasadovargas@gmail.com>
2023-02-13 09:58:05 -0500

    get_tracings_procedures view redone

--- desk/urls.py
+++ desk/urls.py
@@ -8,7 +8,7 @@ app_name = "desk"
 urlpatterns = [
     path("", include(router.urls)),
     path("get_procedures", get_procedures, name="get_procedures"),
-    path("get_tracings_procedures", get_tracings_procedures, name="get_tracings_procedures"),
+    path("get_tracings_procedures/<int:status>", get_tracings_procedures, name="get_tracings_procedures"),
     path("get_dashboard_procedures", get_dashboard_procedures, name="get_dashboard_procedures"),
     path("login/", login, name="login"),
 ]
