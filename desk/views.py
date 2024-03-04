589c981b754b42db4c17a993f046640894713065
jrengifog-epg <jrengifog@unapiquitos.edu.pe>
2024-03-04 07:28:38 -0500

    newdeskpart v1.0.13

--- desk/views.py
+++ desk/views.py
@@ -1103,3 +1103,29 @@ def archive_procedure(request):
         code_number = Procedure.objects.filter(id=procedure_id).first().code_number
 
         return Response(status=status.HTTP_200_OK, data={"code_number": code_number})
+
+
+@api_view(["POST"])
+def anexar_procedure(request):
+    if request.method == "POST":
+        procedure_id = request.data["procedure_id"]
+        user_id = request.data["user_id"]
+        from_area_id = (
+            ProcedureTracing.objects.filter(procedure_id=procedure_id)
+            .last()
+            .from_area_id
+        )
+        ref_procedure_tracking_id = (
+            ProcedureTracing.objects.filter(procedure_id=procedure_id).last().id
+        )
+        ProcedureTracing.objects.create(
+            procedure_id=procedure_id,
+            from_area_id=from_area_id,
+            user_id=user_id,
+            ref_procedure_tracking_id=ref_procedure_tracking_id,
+            is_archived=False,
+            is_finished=False,
+        )
+        code_number = Procedure.objects.filter(id=procedure_id).first().code_number
+
+        return Response(status=status.HTTP_200_OK, data={"code_number": code_number})
