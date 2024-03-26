70d42fa02809b6a5cf9e2486577eef160180544d
Jose Aguilar <joseaguilarsalazar2004@gmail.com>
2024-03-26 12:11:22 -0500

    optimizado dashboard

--- desk/dashboardClasses.py
+++ desk/dashboardClasses.py
@@ -8,16 +8,8 @@ from .serializers import ProcedureSerializer, ProcedureTracingSerializer
 from .getdata import *
 from desk.models import File, Procedure
 from rest_framework import status
-from rest_framework.pagination import PageNumberPagination
-
-class CustomPagination(PageNumberPagination):
-    page_size = 10
-    page_size_query_param = 'page_size'
-    max_page_size = 10
 
 class YourView(APIView):
-   # pagination_class = CustomPagination
-
     def get(self, request):
         usuario_id = request.GET.get("user_id")
         time_filter = request.GET.get("time_filter")
@@ -72,8 +64,10 @@ class YourView(APIView):
 
         ###------------------------------------------------###
 
-        for procedure in procedures:
-            procedure["created_at"] = procedure["created_at"].split(" ")[0]  #tomamos solo la fecha, la hora no nos importa
+        procedures = [
+                {**procedure, "created_at": procedure["created_at"].split(" ")[0]}
+                for procedure in procedures
+            ] 
 
         for l in range(len(procedures)):
                 if procedures[l]['created_at'] not in date_range:   #tomamos solo los tramites que fueron creados en nuestro rango de tiempo
@@ -87,42 +81,41 @@ class YourView(APIView):
         estados = {
             "iniciados": 0,
             "en_proceso": 0, 
-            "archivados": 0, 
-            "concluidos": 0}
+            "archivado": 0, 
+            "concluido": 0}
         
-        dates = defaultdict(lambda: {"finalizados": 0, "archivados": 0, "en_proceso": 0, "iniciados": 0})
+        dates = defaultdict(lambda: {"concluido": 0, "archivado": 0, "en_proceso": 0, "iniciados": 0})
         
         trakins = ProcedureTracingSerializer(ProcedureTracing.objects.filter(procedure_id__in =[procedure["id"] for procedure in procedures]).order_by("-created_at"), many = True).data
 
     # Procesar trakins y procedures juntos
-        for data in [trakins, procedures]:
-            for item in data:
-                print(item)
-                fecha = item["created_at"].split("T")[0].replace("-", "/")
-                año, mes, dia = fecha.split("/")
-                fecha = f"{dia}/{mes}/{año}"
+        for item in trakins:
+            fecha = item["created_at"].split("T")[0].replace("-", "/")
+            año, mes, dia = fecha.split("/")
+            fecha = f"{dia}/{mes}/{año}"
 
-                # Actualizar los conteos correspondientes en el diccionario
-                if item.get("is_finished"):
-                    dates[fecha]["finalizados"] += 1
-                elif item.get("is_archived"):
-                    dates[fecha]["archivados"] += 1
-                else:
-                    dates[fecha]["en_proceso"] += 1
-
+            # Actualizar los conteos correspondientes en el diccionario
+            if item.get("is_finished"):
+                dates[fecha]["concluido"] += 1
+            elif item.get("is_archived"):
+                dates[fecha]["archivado"] += 1
+            else:
+                dates[fecha]["en_proceso"] += 1
 
         for procedure in procedures:
+            fecha = procedure["created_at"].split("T")[0].replace("-", "/")
+            dia, mes, año = fecha.split("/")
+            fecha = f"{dia}/{mes}/{año}"
+
+
             for datel in date_range:
-                fecha = procedure["created_at"].split("T")[0].replace("-", "/")
-                dia, mes, año = fecha.split("/")
-                fecha = f"{dia}/{mes}/{año}"
                 if fecha == datel:
                     dates[datel]['iniciados'] += 1
         i = 0
 
         for l in range(len(dates)):
             try:
-                a = dates[i]["iniciados"] + dates[i]["en_proceso"] + dates[i]["finalizados"] +dates[i]["archivados"]
+                a = dates[i]["iniciados"] + dates[i]["en_proceso"] + dates[i]["concluido"] +dates[i]["archivado"]
                 if a == 0:
                     dates.pop(i)
                 else:
@@ -130,53 +123,134 @@ class YourView(APIView):
             except IndexError:
                 break
 
-        for procedure in procedures:
-            if procedure["state"] != "Archivado" and procedure["state"] != "Concluido":
-                if procedure["state_date"] == 1:
-                    plazos["vencidos"] += 1
-                elif procedure["state_date"] == 2:
-                    plazos["por_vencer"] += 1
-                elif procedure["state_date"] == 3:
-                    plazos["en_plazo"] += 1
+        
+        for procedure in procedures:            
+            state = procedure["state"]
+            state_date = procedure["state_date"]
 
-            if procedure["state"] == "Iniciado":
-                estados["iniciados"] += 1
-            elif procedure["state"] == "Archivado":
-                estados["archivados"] += 1
-            elif procedure["state"] == "Concluido":
-                estados["concluidos"] += 1
-            elif procedure["state"] == "En proceso":
-                estados["en_proceso"] += 1
+            print(state)
+            
+            if state != "Archivado" and state != "Concluido":
+                plazos[
+                    "vencidos" if state_date == 1 else
+                    "por_vencer" if state_date == 2 else
+                    "en_plazo"
+                ] += 1
+
+
+            if state != "Iniciado":
+
+                estados[
+                    state.lower() if state != "En proceso" else "en_proceso"
+                ] += 1
 
         weekGroups = {}
 
-        for key in dates.keys():
+        for key, value in dates.items():
             try:
                 fecha = datetime.strptime(key, "%Y/%m/%d")
             except ValueError:
                 fecha = datetime.strptime(key, "%d/%m/%Y")
 
             week_start = fecha - timedelta(days=fecha.weekday())
+            week_start_str = week_start.strftime("%d/%m/%Y")
 
-            week_start = date.strftime(week_start, "%d/%m/%Y")
-
-            if week_start in weekGroups:
-                weekGroups[week_start]['iniciados'] += dates[key]["iniciados"]
-                weekGroups[week_start]['en_proceso'] += dates[key]["en_proceso"]
-                weekGroups[week_start]['archivados'] += dates[key]["archivados"]
-                weekGroups[week_start]['finalizados'] += dates[key]["finalizados"]
+            if week_start_str in weekGroups:
+                weekGroup = weekGroups[week_start_str]
+                weekGroup['iniciados'] += value["iniciados"]
+                weekGroup['en_proceso'] += value["en_proceso"]
+                weekGroup['archivado'] += value["archivado"]
+                weekGroup['concluido'] += value["concluido"]
             else:
-                weekGroups[week_start] = {"iniciados" : dates[key]["iniciados"],
-                                        "en_proceso" : dates[key]["en_proceso"],
-                                        "archivados": dates[key]["archivados"],
-                                        "finalizados": dates[key]["finalizados"]}
+                weekGroups[week_start_str] = {
+                    "iniciados": value["iniciados"],
+                    "en_proceso": value["en_proceso"],
+                    "archivado": value["archivado"],
+                    "concluido": value["concluido"]
+                }
+
                 
         weekGroupsf = {}
         lista = [week for week in weekGroups.keys()]
         for i in range(len(weekGroups)):
             weekGroupsf[f"Semana{i+1}"] = weekGroups[lista[i]]
 
-        listaAreas = get_dashboard_desk_area(usuario_id, time_filter)
+        
+
+    #--------obtenemos el area del usuario-----------#
+
+        if usuario_id == None:
+            return Response(
+                {"error": "No se encontro el usuario"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        
+        cargo_area = CargoArea.objects.filter(persona__user_id=usuario_id).first()
+        
+        if not cargo_area:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data={"message": "El usuario no tiene un area asignada"},
+            )
+
+        areas = (AreaSerializer(cargo_area.area, many=True).data)
+
+        returnList = []
+
+
+        for area in areas:
+            #----------obtenemos todos los tramites del area--------#
+
+            tracings_for_user = ProcedureTracing.objects.filter(
+                from_area__id=area["id"]).order_by("-created_at")
+
+            procedures = []
+
+            tracings_for_user = ProcedureTracing.objects.select_related('procedure').filter(user_id=usuario_id).order_by("-created_at")
+            i = 0
+
+            procedures = [
+                {**procedure, "created_at": procedure["created_at"].split(" ")[0]}
+                for procedure in procedures
+            ] #tomamos solo la fecha, la hora no nos importa
+
+            procedures = [procedure if procedure['created_at'] in date_range else 0 for procedure in procedures]
+
+
+            thing = [procedure for procedure in procedures if procedure != 0]
+
+            procedures = thing
+            
+            plazos = {"en_plazo": 0, "por_vencer": 0, "vencidos" : 0}
+            estados = {
+                "iniciados": 0,
+                "en_proceso": 0, 
+                "archivado": 0, 
+                "concluido": 0}
+
+
+            for procedure in procedures:            
+                state = procedure["state"]
+                state_date = procedure["state_date"]
+                
+                if state != "Archivado" and state != "Concluido":
+                    plazos[
+                        "vencidos" if state_date == 1 else
+                        "por_vencer" if state_date == 2 else
+                        "en_plazo"
+                    ] += 1
+                if state != "Iniciado":
+                    estados[
+                        state.lower() if state != "En proceso" else "en_proceso"
+                    ] += 1
+                    
+            returnList.append({"area": area["nombre"],
+                            "state_procedure" : estados,
+                            "state_date" : plazos,})
+        
+        listaAreas = returnList
+
+
 
         your_data = {"dates": weekGroupsf,
                      "state_procedure" : estados,
