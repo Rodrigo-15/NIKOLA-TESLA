bf9d97a8c07e1db3adbe60fd95ce37674bf4c441
jrengifog-epg <jrengifog@postgradounap.edu.pe>
2024-04-04 10:53:41 -0500

    bajando cambio

--- desk/signals.py
+++ desk/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import ProcedureTracing, Procedure
-from core.models import CargoArea
+from core.models import CargoArea, Area
 from core.serializers import AreaSerializer
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
@@ -12,14 +12,31 @@ import json
 def procedure_tracing_post_save(sender, instance, created, **kwargs):
     if created:
         channel_layer = get_channel_layer()
-        try:
-            async_to_sync(channel_layer.group_send)(
-                str(instance.user_id),
-                {
-                    "type": "desk_notification",
-                    "message": True,
-                },
-            )
-        except Exception as e:
-            print(e)
+        if instance.to_area_id and not instance.assigned_user_id:
+            area = CargoArea.objects.filter(area__id=instance.to_area_id)
+            # sacar los id de usuarios de la area
+            print(area)
+            for user in area:
+                try:
+                    async_to_sync(channel_layer.group_send)(
+                        str(user.persona.user.id),
+                        {
+                            "type": "desk_notification",
+                            "message": True,
+                        },
+                    )
+                except Exception as e:
+                    print(e)
+
+        elif instance.to_area_id and instance.assign_user_id:
+            try:
+                async_to_sync(channel_layer.group_send)(
+                    str(instance.assigned_user_id),
+                    {
+                        "type": "desk_notification",
+                        "message": True,
+                    },
+                )
+            except Exception as e:
+                print(e)
     pass
