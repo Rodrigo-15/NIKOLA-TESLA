600c52803cb5d438c4533f2b70d13118c0791000
jrengifog-epg <jrengifog@postgradounap.edu.pe>
2024-04-29 08:59:49 -0500

    subiendo ultimos cambios

--- reportes/views.py
+++ reportes/views.py
@@ -48,6 +48,8 @@ from reportes.api import (
     api_get_reporte_actanotas_aplazado_pdf,
     api_get_reporte_academico_alumno_pdf,
     api_get_reporte_economico_alumno_pdf,
+    api_get_reporte_pensiones_programas_excel,
+    api_get_reporte_programas_excel,
 )
 
 
@@ -125,141 +127,6 @@ def get_reporte_programas_api(request):
         'attachment;filename="reporte-programa-{}.xlsx"'.format(Date)
     )
     return response
-
-
-@api_view(["GET"])
-def get_reporte_programas_excel(request):
-    # Type,Date = request.GET.get('dataFetch').split(':')
-    programa_id = request.GET.get("programa_id")
-    promocion = request.GET.get("promocion")
-    anio = request.GET.get("anio")
-    mes = request.GET.get("mes")
-    dia = request.GET.get("dia")
-    # path
-
-    milisecond = str(int(round(time.time() * 1000)))
-    media_root = settings.MEDIA_ROOT
-    excel_folder = os.path.join(media_root, "economicos")
-    if not os.path.exists(excel_folder):
-        os.makedirs(excel_folder)
-    excel_file_name = os.path.join(
-        excel_folder,
-        "reporte-programa-{}-{}-{}-{}-{}.xlsx".format(
-            programa_id, anio, mes, dia, milisecond
-        ),
-    )
-    if os.path.exists(excel_file_name):
-        os.remove(excel_file_name)
-
-    # EXCEL
-    workbook = Workbook(excel_file_name)
-    worksheet = workbook.add_worksheet()
-    # formatFecha=workbook.add_format({'num_format': 'dd/mm/yyyy'})
-    # formatMoney=workbook.add_format({'num_format': '#,##0'})
-    # worksheet.set_column(2,2,cell_format=formatFecha)
-    # worksheet.set_column(3,3,cell_format=formatMoney)
-    header = (
-        "Nombre Completo",
-        "DNI",
-        "N° Operacion",
-        "N° conciliación",
-        "Fecha de Pago",
-        "Monto",
-        "Concepto",
-        "Programa",
-        "Promocion",
-    )
-    worksheet.write_row(0, 0, header)
-
-    Pagos_data = reporte_programa_function(programa_id, promocion, anio, mes, dia)
-
-    rowDefiner = 1
-    monto_total = 0
-    for pag in Pagos_data:
-        worksheet.write_row(rowDefiner, 0, pag)
-        rowDefiner += 1
-        monto_total = monto_total + float(pag[5])
-
-    merge_format = workbook.add_format(
-        {
-            "bold": 1,
-            "border": 1,
-            "align": "center",
-            "valign": "vcenter",
-            "fg_color": "yellow",
-            "num_format": "#,##0",
-        }
-    )
-    linea = rowDefiner + 1
-    worksheet.merge_range("A{}:E{}".format(linea, linea), " MONTO TOTAL", merge_format)
-    worksheet.merge_range("F{}:I{}".format(linea, linea), monto_total, merge_format)
-    workbook.close()
-
-    # return
-    path_return = os.path.join(
-        settings.MEDIA_URL,
-        "economicos",
-        "reporte-programa-{}-{}-{}-{}-{}.xlsx".format(
-            programa_id, anio, mes, dia, milisecond
-        ),
-    )
-    path_return = path_return.replace("\\", "/")
-    return Response({"path": path_return, "cantidad": len(Pagos_data)})
-
-
-def reporte_programa_function(programa_id, promocion, anio, mes, dia):
-    if programa_id == "":
-        if dia == "":
-            pagos_data = Pago.objects.filter(
-                fecha_operacion__year__contains=anio,
-                fecha_operacion__month__contains=mes,
-            )
-        else:
-            pagos_data = Pago.objects.filter(
-                expendiente__promocion__contains=promocion,
-                fecha_operacion__year__contains=anio,
-                fecha_operacion__month__contains=mes,
-                fecha_operacion__day=dia,
-            )
-
-    else:
-        if dia == "":
-            pagos_data = Pago.objects.filter(
-                expendiente__programa_id=programa_id,
-                expendiente__promocion__contains=promocion,
-                fecha_operacion__year__contains=anio,
-                fecha_operacion__month__contains=mes,
-            )
-        else:
-            pagos_data = Pago.objects.filter(
-                expendiente__programa_id=programa_id,
-                expendiente__promocion__contains=promocion,
-                fecha_operacion__year__contains=anio,
-                fecha_operacion__month__contains=mes,
-                fecha_operacion__day=dia,
-            )
-    pagos = []
-    for pago in pagos_data:
-        if bool(pago.expendiente):
-            programa_str = pago.expendiente.programa.nombre
-            promocion_str = pago.expendiente.promocion
-        else:
-            programa_str = ""
-            promocion_str = ""
-        data = (
-            pago.nombre_cliente,
-            pago.numero_documento,
-            pago.numero_operacion,
-            pago.numero_conciliacion,
-            pago.fecha_operacion.strftime("%d/%m/%Y"),
-            pago.monto.__str__(),
-            pago.concepto.nombre,
-            programa_str,
-            promocion_str,
-        )
-        pagos.append(data)
-
-    return pagos
 
 
 @api_view(["GET"])
@@ -674,168 +541,6 @@ def reporte_economico_expediente_api(request):
                 ).replace(",", "."),
             }
         )
-
-
-@api_view(["GET"])
-def get_reporte_pensiones_programas_excel(request):
-    programa_id = request.GET.get("programa_id")
-    promocion = request.GET.get("promocion")
-
-    if programa_id == None:
-        return Response(
-            {"error": "No se encontro el programa"},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    # path
-    milisecond = str(int(round(time.time() * 1000)))
-    media_root = settings.MEDIA_ROOT
-    excel_folder = os.path.join(media_root, "economicos")
-    if not os.path.exists(excel_folder):
-        os.makedirs(excel_folder)
-    excel_file_name = os.path.join(
-        excel_folder,
-        "reporte-pensiones-programa-{}-{}-{}.xlsx".format(
-            programa_id, promocion, milisecond
-        ),
-    )
-    if os.path.exists(excel_file_name):
-        os.remove(excel_file_name)
-
-    # EXCEL
-    workbook = Workbook(excel_file_name)
-    worksheet = workbook.add_worksheet()
-
-    # header
-    format_header = workbook.add_format(
-        {
-            "bold": 1,
-            "border": 1,
-            "align": "center",
-            "valign": "vcenter",
-            "bg_color": "#D9D9D9",
-        }
-    )
-    header = (
-        "N°",
-        "N° Documento",
-        "Nombres",
-        "Primer Apellido",
-        "Segundo Apellido",
-        "Programa",
-        "Promocion",
-        "N° Pensiones",
-        "M. Pensiones",
-        "N° Matriculas",
-        "M. Matriculas",
-        "Observacion",
-    )
-    worksheet.write_row(0, 0, header, format_header)
-
-    # body
-    Pagos_data = reporte_pensiones_programa_function(programa_id, promocion)
-    format_body = workbook.add_format({"border": 1})
-    format_body_number = workbook.add_format({"num_format": "#,##0", "border": 1})
-    format_body_money = workbook.add_format({"num_format": "#,##0.00", "border": 1})
-
-    worksheet.set_column("A:L", None, format_body)
-    worksheet.set_column("I:I", None, format_body_money)
-    worksheet.set_column("H:H", None, format_body_number)
-    worksheet.set_column("J:J", None, format_body_number)
-    worksheet.set_column("K:K", None, format_body_money)
-    # DATA
-    rowDefiner = 1
-    for pag in Pagos_data:
-        worksheet.write_row(rowDefiner, 0, pag)
-        rowDefiner += 1
-
-    workbook.close()
-
-    # return
-    path_return = os.path.join(
-        settings.MEDIA_URL,
-        "economicos",
-        "reporte-pensiones-programa-{}-{}-{}.xlsx".format(
-            programa_id, promocion, milisecond
-        ),
-    )
-    path_return = path_return.replace("\\", "/")
-    return Response({"path": path_return})
-
-
-def reporte_pensiones_programa_function(programa_id, promocion):
-    if programa_id == None:
-        return Response(
-            {"error": "No se encontro el programa"},
-            status=status.HTTP_400_BAD_REQUEST,
-        )
-
-    if promocion == None:
-        expedientes_data = Expediente.objects.filter(
-            programa_id=programa_id, promocion__contains=promocion
-        ).order_by(
-            "persona__apellido_paterno",
-            "persona__apellido_materno",
-        )
-    else:
-        expedientes_data = Expediente.objects.filter(
-            programa_id=programa_id, promocion=promocion
-        ).order_by(
-            "persona__apellido_paterno",
-            "persona__apellido_materno",
-        )
-
-    reportes = []
-    num = 0
-    for exp in expedientes_data:
-        # pensiones
-        monto_pension = Pago.objects.filter(
-            numero_documento=exp.persona.numero_documento,
-            concepto__programa_id=programa_id,
-        ).aggregate(Sum("monto"))["monto__sum"]
-        if monto_pension == None:
-            monto_pension = 0
-        num_pensiones = round((monto_pension / exp.programa.costo), 0)
-
-        # matriculas
-        monto_matriculas = Pago.objects.filter(
-            numero_documento=exp.persona.numero_documento,
-            concepto__codigo="531",
-            monto__in=[201, 251],
-        ).aggregate(Sum("monto"))["monto__sum"]
-        if monto_matriculas == None:
-            monto_matriculas = 0
-        num_matriculas = Pago.objects.filter(
-            numero_documento=exp.persona.numero_documento,
-            concepto__codigo="531",
-            monto__in=[201, 251],
-        ).count()
-
-        # num
-        num += 1
-
-        # observacion
-        observacion = ""
-        if num_pensiones == 0:
-            observacion = "NO TIENE PENSIONES"
-        # data
-        data = (
-            num,
-            exp.persona.numero_documento,
-            exp.persona.nombres,
-            exp.persona.apellido_paterno,
-            exp.persona.apellido_materno,
-            exp.programa.nombre,
-            exp.promocion,
-            num_pensiones,
-            monto_pension,
-            num_matriculas,
-            monto_matriculas,
-            observacion,
-        )
-        reportes.append(data)
-
-    return reportes
 
 
 # new code NEWDESKPART
@@ -1277,3 +982,15 @@ def get_reporte_academico_alumno_pdf(request):
 def get_reporte_economico_alumno_pdf(request):
     if request.method == "POST":
         return api_get_reporte_economico_alumno_pdf(request)
+
+
+@api_view(["GET"])
+def get_reporte_pensiones_programas_excel(request):
+    if request.method == "GET":
+        return api_get_reporte_pensiones_programas_excel(request)
+
+
+@api_view(["GET"])
+def get_reporte_programas_excel(request):
+    if request.method == "GET":
+        return api_get_reporte_programas_excel(request)
