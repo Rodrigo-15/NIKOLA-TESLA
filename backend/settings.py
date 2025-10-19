1dfd4a07e4e781b88482038af57dcc60eabc1838
Rodrigo Paima <rodrickpaima14@gmail.com>
2025-10-19 11:35:32 -0500

    Listo para despliegue en Render

--- backend/settings.py
+++ backend/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
     "default": dj_database_url.config(
         default=env("DATABASE_URL", default="postgresql://postgres:postgres@localhost:5432/postgres"),
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
 
--- backend/urls.py
+++ backend/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     path("api/token/refresh/", TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += static(settings.MEDIA_LOCAL_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
